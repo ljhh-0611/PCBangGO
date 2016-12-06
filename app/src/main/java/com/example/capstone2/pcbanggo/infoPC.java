0b1c41b3d2de96df528a8c9c18b8020c6b18d510
Keunsan <aet978@gmail.com>
2016-12-07 05:44:04 +0900

    스크롤 제한

--- app/src/main/java/com/example/capstone2/pcbanggo/infoPC.java
+++ app/src/main/java/com/example/capstone2/pcbanggo/infoPC.java
@@ -36,22 +36,46 @@ public class infoPC extends AppCompatActivity {
     asyncPHP task;
     String result;
     private int xDelta, yDelta;
+    int[] rows = {22, 16, 9, 13, 13};
+    int[] cols = {17, 9, 18, 10, 12};
+    int row,col;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_pc);
+        Intent intent = getIntent(); // 보내온 Intent를 얻는다
+        Pcroom =intent.getStringExtra("title");
+        switch (Pcroom) {
+            case "3POP":
+                row = rows[0];col=cols[0];break;
+            case "Gallery":
+                row = rows[1];col=cols[1];break;
+            case "Max":
+                row = rows[2];col=cols[2];break;
+            case "Choice":
+                row = rows[3];col=cols[3];break;
+            case "Red":
+                row = rows[4];col=cols[4];break;
+        }
+
 
         View info = (View) getLayoutInflater().inflate(R.layout.info_pc, null);
         final View myinfo = (View) info.findViewById(R.id.myView);
+        final int x1 = 175 +col*75 - 40;
+        final int y1 = 175 +row * 75 - 40;
+        System.out.println("x1, y1" + x1 + " " + y1);
         myinfo.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                final int X = (int) event.getRawX();
-                final int Y = (int) event.getRawY();
+                final int X = (int) (event.getRawX()); // 화면상에서의 좌표
+                final int Y = (int) (event.getRawY());
                 switch (event.getAction() & MotionEvent.ACTION_MASK) {
                     case MotionEvent.ACTION_DOWN:
-                        xDelta = (int) (X - myinfo.getTranslationX());
+                        xDelta = (int) (X - myinfo.getTranslationX()); // delta : 그림상에서의 좌표
                         yDelta = (int) (Y - myinfo.getTranslationY());
                         break;
                     case MotionEvent.ACTION_UP:
@@ -61,22 +85,23 @@ public class infoPC extends AppCompatActivity {
                     case MotionEvent.ACTION_POINTER_UP:
                         break;
                     case MotionEvent.ACTION_MOVE:
-                        myinfo.setTranslationX(X - xDelta);
-                        myinfo.setTranslationY(Y - yDelta);
+                            if ((X - xDelta<=0)&&(X-xDelta>=700-x1))
+                            myinfo.setTranslationX(X - xDelta);
+                            if (Y-yDelta<= 0&&(Y-yDelta>=1000-y1))
+                            myinfo.setTranslationY(Y - yDelta);
+
                         break;
                 }
-
                 return true;
             }
         });
+
         setContentView(info);
 
         TextView pcTitle = (TextView)findViewById(R.id.textView1);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        Intent intent = getIntent(); // 보내온 Intent를 얻는다
-        Pcroom =intent.getStringExtra("title");
         pcTitle.setText(Pcroom);
 
         ImageView img = (ImageView) findViewById(R.id.imageView1);
