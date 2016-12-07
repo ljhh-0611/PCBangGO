e6206e1e0446fdc715932c135c81d4befde44d41
leegeunseong <skscp223@gmail.com>
2016-12-07 18:58:58 +0900

    menu 변경

--- app/src/main/java/com/example/capstone2/pcbanggo/MainActivity.java
+++ app/src/main/java/com/example/capstone2/pcbanggo/MainActivity.java
@@ -194,18 +194,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.contact_us) {
             // Handle the camera action
-            Toast.makeText(this,"camera",Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.make) {
 
         }
 
